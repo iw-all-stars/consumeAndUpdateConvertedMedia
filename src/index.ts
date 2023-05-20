b4c9172b04703e23c25161d67e9347fd14652858
Fantin <fantin@malou.io>
2023-05-20 20:11:35 +0200

    retry

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
 export async function handler(event: any): Promise<any> {
     try {
         console.info("event", event);
+        // find random user
+        const user = await prisma.user.findFirst({});
+        console.info("user", user);
     } catch (e) {
     } finally {
         return;
