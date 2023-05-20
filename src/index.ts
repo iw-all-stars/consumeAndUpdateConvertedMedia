66cdf4cf654c179a8278f26a8770a2865a4ccfb7
Fantin <fantin@malou.io>
2023-05-20 20:47:51 +0200

    add types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { PrismaClient } from "@prisma/client";
+import { S3Event } from "./types";
 
 const prisma = new PrismaClient();
 
-export async function handler(event: any): Promise<any> {
+export async function handler(event: {
+    Records: { s3: S3Event }[];
+}): Promise<any> {
     try {
-        console.info("event", event);
+        console.info("event", event.Records[0].s3);
         // find random user
         const user = await prisma.user.findFirst({});
         console.info("user", user);
