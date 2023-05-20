4387aa01f4c859465be645ac405dfde5136db808
Fantin <fantin@malou.io>
2023-05-20 19:59:02 +0200

    retry

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
 export async function handler(event: any): Promise<any> {
     try {
         console.info("event", event);
